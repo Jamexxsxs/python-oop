0a707b04767fe071866f8b746675a1bbaa928702
Jomary Coquilla <jomarycoquilla465@gmail.com>
2025-01-01 19:03:23 +0800

    .

--- introduction.py
+++ introduction.py
@@ -7,3 +7,12 @@ class Car:
 my_car = Car("Toyota", "Camry")
 print(f"My car is a {my_car.make} {my_car.model}")
 
+# Basic Class with Instance Variables
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+dog1 = Dog("Buddy", "Golden Retriever")
+print(f"{dog1.name} is a {dog1.breed}")
+
