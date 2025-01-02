70c678311bff7dd2f3430d51722803f5f3c0da60
Jomary Coquilla <jomarycoquilla465@gmail.com>
2025-01-02 15:37:38 +0800

    .

--- constructors.py
+++ constructors.py
@@ -9,3 +9,11 @@ book2 = Book("To Kill a Mockingbird", "Harper Lee")
 print(f"Book 1: {book1.title} by {book1.author}")
 print(f"Book 2: {book2.title} by {book2.author}")
 
+# Multiple Instance Variables
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+rect = Rectangle(5, 10)
+print(f"Rectangle has width {rect.width} and height {rect.height}")
